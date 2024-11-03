5b4594c7cbd700b0189377334b3ae0202aba0ea6
julio-ocampo <julio.ocampo@egis-mexico.com>
2024-11-03 01:07:52 -0600

    added dashboard page and iconify dependency and create NavHeader component

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,16 +1,55 @@
 <template>
-    <div>
-      <h1>Dashboard</h1>
+  <div>
+    <NavHeader />
+
+    <div class="grid grid-cols-3 gap-6 p-3 mt-4 text-center">
+      <!-- Card 1 -->
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <div class="mt-4">
+          <h3 class="text-xl font-bold">Activos</h3>
+          <p class="text-gray-600 mt-2">500</p>
+        </div>
+      </div>
+
+      <!-- Card 2 -->
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <div class="mt-4">
+          <h3 class="text-xl font-bold">Activos</h3>
+          <p class="text-gray-600 mt-2">500</p>
+        </div>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <div class="mt-4">
+          <h3 class="text-xl font-bold">Activos</h3>
+          <p class="text-gray-600 mt-2">500</p>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-  });
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NavHeader from "~/components/navigation/NavHeader.vue";
+
+export default defineComponent({
+  components: {
+    NavHeader, // Registrar el componente
+  },
+  methods: {
+    getUsers() {
+      this.$router.push("/users/viewUsers");
+    },
+  },
+  setup() {
+    return {
+      powerPointUrl:
+        "https://1drv.ms/p/s!An6SQ6Ft7VtOq-9GohJx4gExy8QR3w?e=HePPJt",
+    };
+  },
+});
+</script>
+
+<style scoped></style>
