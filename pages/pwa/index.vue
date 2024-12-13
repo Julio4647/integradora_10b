e713ee35c7c1c181c4e9f2d2887a30595e7c72f8
julio-ocampo <julio.ocampo@egis-mexico.com>
2024-12-13 17:14:44 -0600

    update

--- pages/pwa/index.vue
+++ pages/pwa/index.vue
@@ -1,14 +1,11 @@
 <template>
-  <div>
-    <iframe
-      src="/pwa/index.html"
-      style="width: 100%; height: 100vh; border: none"
-      title="PWA App"
-    ></iframe>
-  </div>
-</template>
-<script lang="ts">
-definePageMeta({
-  middleware: "auth",
-});
-</script>
+    <div>
+
+      <iframe
+        src="../../public/pwa/index.html"
+        style="width: 100%; height: 100vh; border: none;"
+        title="PWA App"
+      ></iframe>
+    </div>
+   
+  </template>
