89ecbd5b7507fba0fb7e17a43e31b492f14609e1
VictorRayas <104937978+VictorRayas@users.noreply.github.com>
2024-12-13 16:53:46 -0600

    cambio a la instancia principal

--- pages/pagePWA/index.vue
+++ pages/pagePWA/index.vue
@@ -2,7 +2,7 @@
     <div>
 
       <iframe
-        src="index.html"
+        src="/pwa/index.html"
         style="width: 100%; height: 100vh; border: none;"
         title="PWA App"
       ></iframe>
