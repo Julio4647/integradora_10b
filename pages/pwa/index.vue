7f5d1abccb4de50dd59f4ece808d509c43454589
julio-ocampo <julio.ocampo@egis-mexico.com>
2024-12-13 17:40:44 -0600

    update

--- pages/pwa/index.vue
+++ pages/pwa/index.vue
@@ -1,13 +1,11 @@
-<div id="pwa-container" style="width: 100%; height: 100vh;"></div>
-
 <script>
-  fetch('../../public/pwa/index.html')
-    .then(response => {
-      if (!response.ok) throw new Error('Error al cargar el archivo');
-      return response.text();
-    })
-    .then(html => {
-      document.getElementById('pwa-container').innerHTML = html;
-    })
-    .catch(error => console.error(error));
+export default {
+  mounted() {
+    window.location.href = '../../public/pwa/index.html';
+  }
+};
 </script>
+
+<template>
+  <div>Redirigiendo...</div>
+</template>
