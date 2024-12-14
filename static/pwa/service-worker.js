6510f17ebbf242cb41e237caf272730de8c4c7a4
Brandon Delgado Heredia <120620453+BrandonDelherUT@users.noreply.github.com>
2024-12-13 20:37:55 -0600

    cambios service worker

--- static/pwa/service-worker.js
+++ static/pwa/service-worker.js
@@ -11,11 +11,11 @@ const CACHE_NAME = 'pwa-cache-v8';
 //     'https://cdnjs.cloudflare.com/ajax/libs/pouchdb/7.0.0/pouchdb.min.js',
 // ];
 const urlsToCache = [
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/service-worker.js',
-  '/pouchdb.js',
+  'index.html',
+  'app.js',
+  'manifest.json',
+  'service-worker.js',
+  'pouchdb.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/pouchdb/7.0.0/pouchdb.min.js',
 ];
