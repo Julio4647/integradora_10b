65fdc60e38965e39a52c5411bd8c5d6d80f30812
Brandon Delgado Heredia <120620453+BrandonDelherUT@users.noreply.github.com>
2024-12-13 20:25:46 -0600

    cambio de carpeta pwa

--- pages/pwa/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <iframe
-      src="../static/pwa/index.html"
-      style="width: 100%; height: 100vh; border: none;"
-      title="PWA App"
-    ></iframe>
-  </div>
-</template>
